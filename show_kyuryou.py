e25efb85df9fc79e9518c49f6fe41937c823a2b9
Lafrenze <39109490+Lafreze@users.noreply.github.com>
2020-04-01 18:12:43 +0900

    Update show_kyuryou.py

--- show_kyuryou.py
+++ show_kyuryou.py
@@ -3,7 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 import os
-path="/給料/"
+import numpy as np
+from matplotlib.ticker import MaxNLocator
+import matplotlib.ticker as ticker
+path="C:/給料/"
 #path = args.path
 files= os.listdir(path)
 fi_list = list(files)
@@ -23,38 +26,55 @@ for file in fi_list:
     line_num = len(file_csv[1])
     for li in range(line_num-1):
         for lo in range(loc_num-1):
+            #print(file_csv[lo].loc[li])
             if '時間外勤務手当' in str(file_csv[lo].loc[li]) :
-                zangyou.append(int(str(file_csv[lo+1].loc[li]).replace(',','')))
+                zangyou.append(round(int(str(file_csv[lo+1].loc[li]).replace(',',''))/1000))
             elif '支給内容計' in str(file_csv[lo].loc[li]) :
-                salary.append(int(str(file_csv[lo+1].loc[li]).replace(',','')))
+                salary.append(round(int(str(file_csv[lo+1].loc[li]).replace(',',''))/1000))
             elif '銀行振込額' in str(file_csv[lo].loc[li]) :
-                get_money.append(int(str(file_csv[lo+1].loc[li]).replace(',','')))
-            elif '引去内容計' in str(file_csv[lo].loc[li]) :
-                out_money.append(int(str(file_csv[lo+1].loc[li]).replace(',','')))
+                get_money.append(round(int(str(file_csv[lo+1].loc[li]).replace(',',''))/1000))
+            #elif '引去内容計' in str(file_csv[lo].loc[li]) :
+                #out_money.append(int(str(file_csv[lo+1].loc[li]).replace(',',''))//10000)
             elif '給与明細' in str(file_csv[lo].loc[li]) :
                 date_list.append(file_csv[lo].loc[li+1].replace('\"','').replace('=20',''))
-            elif '所得税' in str(file_csv[lo].loc[li]) :
-                tax_list.append(int(str(file_csv[lo+1].loc[li]).replace(',','')))
+            #elif '所得税' in str(file_csv[lo].loc[li]) :
+                #tax_list.append(int(str(file_csv[lo+1].loc[li]).replace(',',''))//10000)
     if len(zangyou)<len(date_list):
         zangyou.append(0)
 
 
 matplotlib.rcParams['font.family']='SimSun'
-plt.plot(date_list, salary,label="支給内容計",marker="o")
-plt.plot(date_list, get_money,label="銀行振込額",marker="o")
-plt.plot(date_list, out_money,label="引去内容計",marker="o")
-plt.plot(date_list, tax_list,label="所得税",marker=".")
-plt.plot(date_list, zangyou,label="時間外勤務手当",marker=".")
+fig, ax1 = plt.subplots()
+ax1.plot(date_list, salary,color='b',label="支給内容計",marker="o")
+ax1.plot(date_list, get_money,color='lightblue',label="銀行振込額",marker="o")
+#ax1.plot(date_list, out_money,label="引去内容計",marker="o")
+#ax1.plot(date_list, tax_list,label="所得税",marker=".")
+ax1.plot(date_list, zangyou,label="時間外勤務手当",marker="o")
+ax2 = ax1.twinx()
+sum_salary = np.cumsum(salary)
+sum_get_money = np.cumsum(get_money)
+ax2.plot(date_list,sum_salary,color='pink',label="支給総額",marker="x")
+ax2.plot(date_list,sum_get_money,color='magenta',label="支給総額",marker="x")
+
 for a,b in zip(date_list,salary):
-    plt.annotate('%s'%b,xy=(a,b),xytext=(-20,10),
-                 textcoords='offset points')
+    ax1.annotate('%s'%b,xy=(a,b),xytext=(-20,10),textcoords='offset points')
 for a,b in zip(date_list,get_money):
-    plt.annotate('%s'%b,xy=(a,b),xytext=(-20,10),
-                 textcoords='offset points')
+    ax1.annotate('%s'%b,xy=(a,b),xytext=(-20,10),textcoords='offset points')
 for a,b in zip(date_list,zangyou):
-    plt.annotate('%s'%b,xy=(a,b),xytext=(-20,10),
-                 textcoords='offset points')
-plt.legend()
-plt.xlabel('月',fontproperties='SimSun',fontsize=9)  
-plt.ylabel('円',fontproperties='SimSun',fontsize=9)
+    ax1.annotate('%s'%b,xy=(a,b),xytext=(-20,10),textcoords='offset points')
+for a,b in zip(date_list,sum_salary):
+    ax2.annotate('%s'%b,xy=(a,b),xytext=(-20,10),textcoords='offset points')
+for a,b in zip(date_list,sum_get_money):
+    ax2.annotate('%s'%b,xy=(a,b),xytext=(-20,10),textcoords='offset points')
+handler1, label1 = ax1.get_legend_handles_labels()
+handler2, label2 = ax2.get_legend_handles_labels()
+ax1.legend(handler1 + handler2, label1 + label2, loc=2, borderaxespad=0.)
+
+pageview_max = max(salary)
+register_max = max(sum_salary)
+
+ax1.set_ylim([0, pageview_max])
+ax2.set_ylim([0, register_max])
+plt.xlabel('月',fontproperties='SimSun',fontsize=9)
+plt.ylabel('円(千)',fontproperties='SimSun',fontsize=9)
 plt.show()
